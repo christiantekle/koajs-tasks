06b9458266a8c9d876daf64ba39d61733678311a
christiantekle <christiantekle@users.noreply.github.com>
2020-07-08 02:17:15 +0400

    update function implemented

--- controller/taskController.js
+++ controller/taskController.js
@@ -28,11 +28,27 @@ const deleteTask = async (ctx) => {
     await Task.deleteOne({
       _id: ctx.params.id,
     });
-    ctx.body = { status: "Task Deleted" };
+    ctx.body = { status: "Task Deleted!" };
   } catch (err) {
     ctx.body = "Error: " + err;
   }
 };
-module.exports.deleteTask = deleteTask;
-module.exports.postTask = postTask;
+
+const putTask = async (ctx) => {
+  if (!ctx.request.body.task_name) {
+    ctx.body = { error: "Bad Data" };
+  }
+  try {
+    await Task.findOneAndUpdate(
+      { _id: ctx.params.id },
+      { task_name: ctx.request.body.task_name }
+    );
+    ctx.body = { status: "Task Updated" };
+  } catch (err) {
+    ctx.body = "error: " + err;
+  }
+};
 module.exports.getTasks = getTasks;
+module.exports.postTask = postTask;
+module.exports.deleteTask = deleteTask;
+module.exports.putTask = putTask;
--- routes/tasks.js
+++ routes/tasks.js
@@ -5,5 +5,5 @@ const taskController = require("../controller/taskController");
 router.get("/api/tasks", taskController.getTasks);
 router.post("/api/tasks", taskController.postTask);
 router.delete("/api/tasks/:id", taskController.deleteTask)
-
+router.put("/api/tasks/:id", taskController.putTask)
 module.exports = router;
