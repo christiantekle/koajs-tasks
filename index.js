e42706cbac668135199704b8679b8dfc44f77397
christiantekle <christiantekle@users.noreply.github.com>
2020-07-07 20:08:29 +0400

    index.js file created

---
+++ index.js
@@ -0,0 +1,15 @@
+const Koa = require("koa");
+const app = new Koa();
+const bodyParser = require("koa-body");
+const mongoose = require("mongoose");
+
+const tasks = require("../routes/tasks");
+
+app.use(bodyParser());
+app.use(tasks.routes());
+
+mongoose.connect("mongodb://localhost:koajstasks", { useNewUrlParser });
+
+app.listen(5000, () => {
+  console.log("Server running at port 5000");
+});
--- models/Task.js
+++ models/Task.js
@@ -1,10 +1,10 @@
-const mongoose = require('mangoose')
-const Schema = mongoose.Schema
+const mongoose = require("mangoose");
+const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    task_name: {
-        type: String
-    }
-})
+  task_name: {
+    type: String,
+  },
+});
 
-module.exports = Task = mongoose.model('tasks', TaskSchema)
+module.exports = Task = mongoose.model("tasks", TaskSchema);
